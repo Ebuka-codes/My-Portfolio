ab10e00715b764a54fd8c418c96e848ea84ced18
Ebuka-codes <nwubasilas@yahoo.com>
2026-04-14 19:45:40 +0100

    fix bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
-  safelist: [
-    {
-      pattern: /.*/, // keep all Tailwind classes (temporary fix)
-    },
-  ],
+
   theme: {
     extend: {
       colors: {
